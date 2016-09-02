f902d5589291101fc05307947194d57b3d777df6
Robert Benjamin <rotberj@gmail.com>
2016-09-02 14:02:21 -0700

    Remove baffle.js until it's working properly

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
+/*
 (function() {
   document.querySelector('.links').addEventListener('mouseover', function(e) {
-    if (!e.target.classList.contains('links')) window.baffle(e.target).start().reveal(3000);
+    if (!e.target.classList.contains('links')) {
+      window.baffle(e.target).reveal().start().reveal(250);
+    }
   });
   document.querySelector('.email-paragraph a').addEventListener('mouseover', function(e) {
-    window.baffle(e.target).start().reveal(1000);
+    window.baffle(e.target).reveal().start().reveal(250);
   });
 })();
+*/
