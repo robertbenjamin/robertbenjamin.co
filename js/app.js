83502d177591da6bcf3df11d686a01ccd2fca621
Robert Benjamin <rotberj@gmail.com>
2016-10-14 23:02:59 -0700

    Pull in changes from the gh-pages branch (#1)
    * Create CNAME
    
    * Add basic mobile styling, add favicon
    
    * Mobile fix, more styling
    
    * That damn box sizing
    
    * Add baffle.js animations
    
    * Remove baffle.js until it's working properly
    
    * Update styling
    
    * Update favicon
    
    * Add link to books project

---
+++ js/app.js
@@ -0,0 +1,12 @@
+/*
+(function() {
+  document.querySelector('.links').addEventListener('mouseover', function(e) {
+    if (!e.target.classList.contains('links')) {
+      window.baffle(e.target).reveal().start().reveal(250);
+    }
+  });
+  document.querySelector('.email-paragraph a').addEventListener('mouseover', function(e) {
+    window.baffle(e.target).reveal().start().reveal(250);
+  });
+})();
+*/
